36582d5f9b8a3ad77225588460469c361a200deb
vaati2805 <dantovahati@gmail.com>
2026-03-26 22:58:45 +0300

    Fix: add delay for db connection and clear caches

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,9 +5,16 @@ set -e
 php artisan storage:link --force 2>/dev/null || true
 
 # Cache config & routes for performance
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+
+# Wait for the database to be ready
+echo "Waiting for database..."
+sleep 15
 
 # Run database migrations
 php artisan migrate --force
